2a8eeb88c1fd7c94d83c75266ef9562784f1545b
Oleg Obidin <oleg.obidin@revolut.com>
2020-07-11 19:10:34 +0100

    Add dotenv

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
