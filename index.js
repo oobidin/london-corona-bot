a48878c4e3a8afdc24367053f5a67f230246a00d
Oleg Obidin <oleg.obidin@revolut.com>
2020-07-12 12:58:24 +0100

    Move to webhooks

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const { Telegraf } = require('telegraf')
-const fs = require('fs-extra')
 const request = require('axios')
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const { TELEGRAM_TOKEN, PORT = 3000, URL } = process.env
 
 const CSV_URL =
   'https://data.london.gov.uk/download/coronavirus--covid-19--cases/7a3c305c-fa4e-47db-8843-aed537cde495/phe_cases_london_england.csv'
 
-const downloadData = async () => {
+const bot = new Telegraf(TELEGRAM_TOKEN)
+
+const downloadAndFormatData = async () => {
   try {
     const { data } = await request(CSV_URL)
 
@@ -30,12 +31,16 @@ const downloadData = async () => {
   }
 }
 
+const webhookUrl = `bot/${TELEGRAM_TOKEN}`
+
+bot.telegram.setWebhook(`${URL}/${webhookUrl}`)
+
 bot.hears(['data', 'Data'], async (ctx) => {
   ctx.reply('Thinking...')
 
-  const data = await downloadData()
+  const data = await downloadAndFormatData()
 
   ctx.reply(data)
 })
 
-bot.launch()
+bot.startWebhook(`/${webhookUrl}`, null, PORT)
